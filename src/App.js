3a5a479dbec641e039a738e537a46160762eadf8
Thiago Guimarães <thiago.guimaraes@conectt.com.br>
2019-10-11 16:47:14 -0300

    signInAnonymously

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import firebase from 'react-native-firebase';
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      isAuthenticated: false,
+    };
+  }
+
+  componentDidMount() {
+    firebase.auth().signInAnonymously()
+      .then(() => {
+        this.setState({
+          isAuthenticated: true,
+        });
+      });
+  }
+
+  render() {
+    // If the user has not authenticated
+    if (!this.state.isAuthenticated) {
+      return null;
+    }
+
+    return (
+      <View>
+        <Text>Welcome to my awesome app!</Text>
+      </View>
+    );
+  }
+
+}
+
+export default App;
