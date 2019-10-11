8057ecb4c9463b9a138dbff52f237de84f4d7c0b
Thiago Guimarães <thiago.guimaraes@conectt.com.br>
2019-10-11 17:55:05 -0300

    login

--- src/App.js
+++ src/App.js
@@ -1,38 +1,82 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, AsyncStorage, Button, StyleSheet } from 'react-native';
 import firebase from 'react-native-firebase';
 
+import Login from './screens/Login'
 class App extends React.Component {
 
   constructor() {
     super();
+    this.unsubscriber = null;
     this.state = {
-      isAuthenticated: false,
-    };
+        user: null,
+        errorMessage: null
+    }
+  }
+  /**
+   * Observa qualquer mudança que aconteca no estado da autenticacao e atualiza o estado do usuário
+   */
+  componentDidMount() {
+    this.unsubscriber = firebase.auth().onAuthStateChanged((user) => {
+        this.setState({ user });
+    })
   }
 
-  componentDidMount() {
-    firebase.auth().signInAnonymously()
-      .then(() => {
-        this.setState({
-          isAuthenticated: true,
-        });
-      });
+  componentWillUnmount() {
+    if(this.unsubscriber) {
+        this.unsubscriber();
+    }
+  }
+
+  signIn = async () => {
+      try {
+          
+        const response = {
+            data: {
+                user: {
+                    email: '',
+                    password: ''
+                },
+                token: '',
+                errorMessage: 'User not found'
+            }
+        }
+        const { user, token } = response.data;
+        
+        throw response
+
+        await AsyncStorage.multiSet([
+            ['@CodeApi:token', token],
+            ['@CodeApi:user', JSON.stringify(user)],
+        ])
+      } catch (response) {
+          this.setState({ errorMessage: response.data.error })
+          
+      }
   }
 
   render() {
-    // If the user has not authenticated
-    if (!this.state.isAuthenticated) {
-      return null;
+    console.log(this.state.user)
+    if (!this.state.user) {
+        return <Login />;
     }
 
     return (
-      <View>
-        <Text>Welcome to my awesome app!</Text>
+      <View style={styles.container}>
+          {this.state.errorMessage && <Text>{ this.state.errorMessage }</Text> }
+        {/* <Text>Bem-vindo {this.state.user.email}!</Text> */}
+        <Button onPress={this.signIn} title="Entrar"></Button>
       </View>
     );
   }
-
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
 
 export default App;
